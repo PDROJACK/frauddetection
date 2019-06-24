5c0bd86e6cb8eafd921d83f6b1aaec7eda97d76a
Priyadasrhan singh <singhpd75@gmail.com>
2019-06-24 12:16:40 +0530

    updated getRsultSet

--- src/main/java/com/airtel/frauddetection/GetResultSet.java
+++ src/main/java/com/airtel/frauddetection/GetResultSet.java
@@ -1,5 +1,7 @@
 package com.airtel.frauddetection;
 
+import com.airtel.frauddetection.filter.Filter;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -14,7 +16,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.sun.xml.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
 import org.json.simple.JSONArray;
 import org.json.simple.parser.*;
@@ -25,7 +26,6 @@ public class GetResultSet {
         String url = "jdbc:mysql://localhost:3306/transactions?useSSL=false";
         String username = "root";
         String password = "PDrojack@455";
-
         Connection conn = DriverManager.getConnection(url, username, password);
         return conn;
     }
@@ -34,56 +34,64 @@ public class GetResultSet {
         Connection conn = null;
         Statement stmt = null;
         ResultSet res = null;
+        List<Map<String, Object>> rows = new ArrayList<Map<String, Object>>();
         try {
             conn = getConnection();
             System.out.println("Conn=" + conn);
             String query = "SELECT * from transactions";
             stmt = conn.createStatement();
             res = stmt.executeQuery(query);
-            System.out.println(resultSetToList(res));
+            try {
+                rows = Filter.getFilteredData(res);
+                System.out.println(rows);
+            } catch (SQLException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
         } catch (Exception e) {
             e.printStackTrace();
             System.exit(1);
         }
     }
-
-    private static List<Map<String, Object>> resultSetToList(ResultSet rs) throws SQLException {
-        ResultSetMetaData md = rs.getMetaData();
-        int columns = md.getColumnCount();
-        List<Map<String, Object>> rows = new ArrayList<Map<String, Object>>();
-        JSONArray json = new JSONArray();
-        List<String> list=new ArrayList<String>();
-        Object obj;
-        try {
-            obj = new JSONParser().parse(new FileReader(
-                    "G:\\Eclipse\\frauddetection\\src\\main\\java\\com\\airtel\\frauddetection\\filter.json"));
-            json = (JSONArray) obj;
-            list = json;
-        } catch (ParseException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        while (rs.next()){
-            Map<String, Object> row = new HashMap<String, Object>(columns);
-            //Looping over the items
-            for(int i = 1; i <= columns; ++i){
-                for(String o: list){
-                   if(md.getColumnLabel(i).equals(o)){
-                       row.put(md.getColumnLabel(i),rs.getObject(i));
-                   }
-                    // System.out.println(md.getColumnName(i));
-                   // System.out.println((String)json.get(j));
-                }
-            } 
-            //Map<String, Object> result = row.entrySet().stream().filter(map->map.getKey().equals("customer_no")).collect(Collectors.toMap(map->map.getKey(),map->map.getValue()));
-            rows.add(row);
-        }
-        return rows;
-    } 
 }
+
+//     private static List<Map<String, Object>> resultSetToList(ResultSet rs) throws SQLException {
+//         ResultSetMetaData md = rs.getMetaData();
+//         int columns = md.getColumnCount();
+//         List<Map<String, Object>> rows = new ArrayList<Map<String, Object>>();
+//         JSONArray json = new JSONArray();
+//         List<String> list=new ArrayList<String>();
+//         Object obj;
+//         try {
+//             obj = new JSONParser().parse(new FileReader(
+//                     "G:\\Eclipse\\frauddetection\\src\\main\\java\\com\\airtel\\frauddetection\\filter\\utils\\filter.json"));
+//             json = (JSONArray) obj;
+//             list = json;
+//         } catch (ParseException e) {
+//             // TODO Auto-generated catch block
+//             e.printStackTrace();
+//         } catch (FileNotFoundException e) {
+//             // TODO Auto-generated catch block
+//             e.printStackTrace();
+//         } catch (IOException e) {
+//             // TODO Auto-generated catch block
+//             e.printStackTrace();
+//         }
+//         while (rs.next()){
+//             Map<String, Object> row = new HashMap<String, Object>(columns);
+//             //Looping over the items
+//             for(int i = 1; i <= columns; ++i){
+//                 for(String o: list){
+//                    if(md.getColumnLabel(i).equals(o)){
+//                        row.put(md.getColumnLabel(i),rs.getObject(i));
+//                    }
+//                     // System.out.println(md.getColumnName(i));
+//                    // System.out.println((String)json.get(j));
+//                 }
+//             } 
+//             //Map<String, Object> result = row.entrySet().stream().filter(map->map.getKey().equals("customer_no")).collect(Collectors.toMap(map->map.getKey(),map->map.getValue()));
+//             rows.add(row);
+//         }
+//         return rows;
+//     } 
+// }
