2a367374ed2b5211125f3273ff98ae7c84f3e426
Priyadasrhan singh <singhpd75@gmail.com>
2019-07-09 13:05:36 +0530

    changes

--- src/main/java/com/airtel/frauddetection/GetHistoricalData.java
+++ src/main/java/com/airtel/frauddetection/GetHistoricalData.java
@@ -3,18 +3,13 @@ package com.airtel.frauddetection;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-import javax.swing.text.DocumentFilter;
-
 import com.airtel.frauddetection.model.RetailerModel;
-import com.mongodb.DBObject;
-import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.Filters;
-import com.mongodb.MongoClient;
+
 
 import org.bson.Document;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -27,34 +22,36 @@ import java.util.List;
  */
 public class GetHistoricalData {
 
-    public static List<RetailerModel> historicalData() throws Exception {
+    public static MongoCollection<Document> collections(String user,String collec_name, String dbname, String pwd){
         MongoClient mongo = new MongoClient("localhost", 27017);
-
         MongoCredential credential;
-        credential = MongoCredential.createCredential("pdrojack", "retailers", "12345678".toCharArray());
-
-        MongoDatabase database = mongo.getDatabase("retailers");
-        MongoTemplate mongoTemplate = new MongoTemplate(mongo, "retailers");
+        credential = MongoCredential.createCredential(user, dbname, pwd.toCharArray());
+        MongoDatabase database = mongo.getDatabase(dbname);
+        MongoTemplate mongoTemplate = new MongoTemplate(mongo, dbname);
         // Find
-        MongoCollection<Document> collection = database.getCollection("retailer_data");
-        Document query = new Document();
+        MongoCollection<Document> collection = database.getCollection(collec_name);
+        return collection;      
+    }
 
+    public static MongoTemplate getMongoTemplate(String dbname) {
+        MongoClient mongo = new MongoClient("localhost",27017);
+        MongoDatabase database = mongo.getDatabase(dbname);
+        MongoTemplate mongoTemplate = new MongoTemplate(mongo, dbname);
+        return mongoTemplate;
+    }
 
-
+    public static List<RetailerModel> historicalData() throws Exception {
+        MongoCollection<Document> collection = collections("pdrojack", "retailer_data", "retailers", "12345678");
         FindIterable<Document> doc = collection.find();
         int i = 1;
-       // FindIterable<Document> doc = collection.find(Filters.eq("retailer_id", 1));
-        //List<RetailerModel> returnList = collection.find(query,RetailerModel.class).into(new ArrayList<RetailerModel>());
         List<RetailerModel> returnList = new ArrayList<RetailerModel>();
-        
         Iterator it = doc.iterator();
-
+        MongoTemplate mongoTemplate = getMongoTemplate("retailers");
         while(it.hasNext()){
             Document obj = (Document) it.next();
             RetailerModel retailer = mongoTemplate.getConverter().read(RetailerModel.class, obj);
             returnList.add(retailer);
         }
-        
         //System.out.println(returnList.get(0).getDaily_avg());
         return returnList;
 
--- src/main/java/com/airtel/frauddetection/services/Computation.java
+++ src/main/java/com/airtel/frauddetection/services/Computation.java
@@ -1,42 +1,60 @@
 package com.airtel.frauddetection.services;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import com.airtel.frauddetection.filter.utils.FilterReader;
 import com.airtel.frauddetection.model.DataPojo;
 import com.airtel.frauddetection.model.RetailerModel;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
 
 import org.codehaus.janino.ExpressionEvaluator;
 import org.codehaus.janino.Parser.ParseException;
+import org.bson.Document;
 import org.codehaus.janino.CompileException;
 import org.codehaus.janino.Scanner.ScanException;
-
+import com.airtel.frauddetection.GetHistoricalData;
 /**
  * Computation
  */
 public class Computation {
     static private String[] expression = FilterReader.getExpression();
+    static private MongoCollection<Document> collection = GetHistoricalData.collections("pdrojack", "retailer_data", "retailers", "12345678");
     
-    public static void expressionEvaluator(List<Map<String, DataPojo<?>>> filteredData, List<RetailerModel> historicalData)
+    public static List<Map<Integer,Integer>> expressionEvaluator(List<Map<String, DataPojo<?>>> filteredData, List<RetailerModel> historicalData)
     throws CompileException, ParseException, ScanException, InvocationTargetException {
         
+        List<Map<Integer,Integer>> newData = new ArrayList<Map<Integer,Integer>>();
         Class cls = filteredData.get(1).get("amount").getValue().getClass();
+
         ExpressionEvaluator ee = new ExpressionEvaluator();
+        
         ee.setParameters(new String[]{"amount","daily_avg"}, new Class[]{cls,Integer.class});
+        
         ee.setExpressionType(Integer.class);
+        
         ee.cook(expression[0]);
                 
         for(Map<String,DataPojo<?>> data : filteredData ){
             for(RetailerModel retailer : historicalData) {
                 if(retailer.getRetailer_id()==(Integer) data.get("retailer_id").getValue()){
+                    Map<Integer,Integer> newVal = new HashMap<Integer,Integer>();
+                    Integer id = (Integer)data.get("retailer_id").getValue();
                     Object value = (Integer) data.get("amount").getValue();
                     Integer daily_avg = (Integer) retailer.getDaily_avg();
-                    System.out.println(ee.evaluate(new Object[]{ value, daily_avg }));
+                    Integer result = (Integer)ee.evaluate(new Object[]{ value, daily_avg });
+                    newVal.put(id, result);
+                    collection.updateOne(Filters.eq("retailer_id",id), Updates.set("daily_avg",result));;
+                    newData.add(newVal);
                     }
                 }
             }
+            return newData;
         }
     }
 
